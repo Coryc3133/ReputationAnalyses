1efcf0ac0cd30aef9b719d051022859b8dbf2fa4
Cory Costello <coryc3133@gmail.com>
2018-06-06 23:09:48 -0700

    Finished the first stab at the ReputationAnalyses package. Currently only ahas three functions and a wrapper function, but many plans for additions.

---
+++ R/rep_sim_data.R
@@ -0,0 +1,17 @@
+#' Simulated Reputation data
+#'
+#' Simulated data to be used for examples with the ReputationAnalyses package.
+#' The data were simulate based on results from Costello & Srivastava (in prep).
+#' We used the simsem package to simulate a dataset based on a population model for hearsay reputations.
+#' The parameter estimates from analyses pooling across the two studies
+#' conducted on hearsay reputations were treated as the population parameters for simulating the data.
+#'
+#' The data, being simulated, should be treated solely as example data.
+#'
+#' @docType data
+#'
+#' @usage data(rep_sim_data)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
