6ec16d05ae86bed6fc465e072b8caaf6b2e83140
Cory Costello <coryc3133@gmail.com>
2019-04-11 23:35:50 -0700

    Started adding group equality options to fit functions; left off at a working version for consensus only

--- R/group_mods.R
+++ R/group_mods.R
@@ -146,7 +146,7 @@ rep_consensus_group_mod_builder <- function(p1_reports, p2_reports, groups = NUL
 
         model <-
           # hearsay (P1-P2) consensus
-          paste(paste(p1_reports, param_labs["hc"], "*", p2_reports),
+          paste(paste(p1_reports, "~~", param_labs["hc"], "*", p2_reports),
                 # intercepts
                 paste(p1_reports, "~", param_labs["int_p1"], "*1"),
                 paste(p2_reports, "~", param_labs["int_p2"], "*1"),
@@ -167,12 +167,12 @@ rep_consensus_group_mod_builder <- function(p1_reports, p2_reports, groups = NUL
           # cross-target correlations
           if(i < n_triads){
             prev_i <- i:1
-            m   <- paste(p1_reports[i], param_labs["m"], "*", p2_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
-            rec <- paste(p1_reports[i], param_labs["rec"], "*", p1_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
-            h   <-  paste(p2_reports[i],param_labs["h"], "*", p2_reports[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
+            m   <- paste(p1_reports[i], "~~", param_labs["m"], "*", p2_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
+            rec <- paste(p1_reports[i], "~~", param_labs["rec"], "*", p1_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
+            h   <-  paste(p2_reports[i],"~~", param_labs["h"], "*", p2_reports[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
             xtrs <- paste(m, rec, h, sep = "\n")
             model <- paste(model, xtrs)}}
-        hc <- paste(p1_reports, param_labs["hc"], "*", p2_reports) %>% stringr::str_flatten(collapse = "\n")
+        hc <- paste(p1_reports, "~~", param_labs["hc"], "*", p2_reports) %>% stringr::str_flatten(collapse = "\n")
         int_p1 <- paste(p1_reports, "~", param_labs["int_p1"], "*1") %>% stringr::str_flatten(collapse = "\n")
         int_p2 <- paste(p2_reports, "~", param_labs["int_p2"], "*1") %>% stringr::str_flatten(collapse = "\n")
         v_p1 <- paste(p1_reports, "~~", param_labs["v_p1"], "*", p1_reports) %>% stringr::str_flatten(collapse = "\n")
@@ -234,6 +234,14 @@ rep_consensus_group_mod_builder <- function(p1_reports, p2_reports, groups = NUL
 #' If more than one is supplied, the target-wise order must match the other
 #' rating types.
 #' @param group_mod The quoted column name that contains a group-level categorical moderator.
+#' @param use_labs Logical indicating whether or not to use the group labels to create the parameter labels.
+#' If FALSE, generic labels (grp1 to grpk, where k is the number of groups) are used.
+#' @param groups_eql Optional. Groups that you want to constrain to be equal across some or all parameters. If
+#' you have use_labs set to TRUE, provide a vector of group labels corresponding to the groups you want to constrain to be equal. If you
+#' have use_labs set to FALSE, provide a vector of numbers corresponding to the position of the groups you want to constrain to be equal.
+#' @param params_eql Optional. Parameters that you want to constrain to be equal across the groups specified in groups_eql. You can provide
+#' one or more specific parameters (e.g., "hc" for hearsay consensus), or use one of several built-in options including "all" which constrains
+#' all parameters to be equal across groups and "main" which constrains just the hearsay consensus to be equal across groups (in this model).
 #' @param n_triads The number of exchangeable triads in each group. By default, this is determined by
 #' counting the number of P1 reports. This parameter rarely needs to be changed.
 #' @param n_p1s_per_p2s The number of P1s for every P2. This defaults to 1.
@@ -259,19 +267,32 @@ rep_consensus_group_mod_builder <- function(p1_reports, p2_reports, groups = NUL
 #'
 #' @return The function returns an object of class \code{\link[lavaan:lavaan-class]{lavaan}}.
 
-rep_consensus_group_mod <- function(data, model = NULL, p1_reports, p2_reports, group_mod = NULL, n_triads = length(p1_reports),
-                          n_p1s_per_p2s = 1, n_p2s_per_p1s = 1){
+rep_consensus_group_mod <- function(data, model = NULL, p1_reports, p2_reports,
+                                    group_mod = NULL, use_labs = TRUE, groups_eql = NULL, params_eql = "none",
+                                    n_triads = length(p1_reports),
+                                    n_p1s_per_p2s = 1, n_p2s_per_p1s = 1){
   if(is.null(group_mod)){warning("You need to supply a group variable to run a group-moderator Reputation Model.
                             If you don't have a group moderator, try rep_consensus if you have no moderator or
                             rep_id_mods_consensus if you have an individual difference moderator.")
   }
   else{
     groups <- levels(as.factor(data[,group_mod]))
+    groups <- groups %>% str_remove_all("_")
     if(is.null(model)){
-      rep_consensus_groups_model <- rep_consensus_group_mod_builder(p1_reports, p2_reports, groups, n_triads = length(p1_reports),
+      rep_consensus_groups_model <- rep_consensus_group_mod_builder(p1_reports, p2_reports, groups, use_labs = use_labs,
+                                                                    n_triads = length(p1_reports),
                                                                     n_p1s_per_p2s = 1, n_p2s_per_p1s = 1)}
-
     else{rep_consensus_groups_model <- model}
+
+    if(groups_eql != "none"){
+      groups_eql <- groups_eql %>% str_remove_all("_")
+      eql_labs <- expand.grid(groups_eql, params_eql)
+      old_labs <- paste(eql_labs$Var1, eql_labs$Var2, sep = "_")
+      new_labs <- eql_labs$Var2 %>% as.character()
+      names(new_labs) <- old_labs
+      rep_consensus_groups_model$model <- str_replace_all(rep_consensus_groups_model$model, new_labs)
+    }
+
     fitted_model <- lavaan::sem(rep_consensus_groups_model$model, data = data, missing = "FIML", group = group_mod)
     return(fitted_model)}
   }
@@ -449,11 +470,11 @@ rep_con_acc_group_mod_builder <- function(p1_reports, p2_reports, target_self,
 
         model <-
                 # hearsay consensus (P1-P2 agreement)
-          paste(paste(p1_reports, param_labs["hc"], "*", p2_reports),
+          paste(paste(p1_reports, "~~", param_labs["hc"], "*", p2_reports),
                 # hearsay accuracy (P2-self agreement)
-                paste(p2_reports, param_labs["ha"], "*", target_self),
+                paste(p2_reports,"~~", param_labs["ha"], "*", target_self),
                 #direct accuracy (P1-self agreement)
-                paste(p1_reports, param_labs["da"], "*", target_self),
+                paste(p1_reports,"~~", param_labs["da"], "*", target_self),
 
 
 
@@ -480,18 +501,18 @@ rep_con_acc_group_mod_builder <- function(p1_reports, p2_reports, target_self,
           # cross-target correlations
           if(i < n_triads){
             prev_i <- i:1
-            m   <- paste(p1_reports[i], param_labs["m"], "*", p2_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
-            rec <- paste(p1_reports[i], param_labs["rec"], "*", p1_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
-            h   <-  paste(p2_reports[i], param_labs["h"], "*", p2_reports[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
-            tru_sim <- paste(target_self[i], param_labs["tru_sim"], "*", target_self[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
-            as_sim_3p <- paste(p2_reports[i], param_labs["as_sim_3p"], "*", target_self[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
-            as_sim_1p <- paste(p1_reports[i], param_labs["as_sim_1p"], "*", target_self[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
+            m   <- paste(p1_reports[i],       "~~", param_labs["m"], "*", p2_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
+            rec <- paste(p1_reports[i],       "~~", param_labs["rec"], "*", p1_reports[-prev_i]) %>% stringr::str_flatten(collapse = "\n")
+            h   <-  paste(p2_reports[i],      "~~", param_labs["h"], "*", p2_reports[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
+            tru_sim <- paste(target_self[i],  "~~", param_labs["tru_sim"], "*", target_self[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
+            as_sim_3p <- paste(p2_reports[i], "~~", param_labs["as_sim_3p"], "*", target_self[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
+            as_sim_1p <- paste(p1_reports[i], "~~", param_labs["as_sim_1p"], "*", target_self[-prev_i], "\n") %>% stringr::str_flatten(collapse = "\n")
             xtrs <- paste(m, rec, h, tru_sim, as_sim_3p, as_sim_1p, sep = "\n")
             model <- paste(model, xtrs)}}
         # params shared with single triad model (within triad correlations)
-        hc <- paste(p1_reports, param_labs["hc"], "*", p2_reports) %>% stringr::str_flatten(collapse = "\n")
-        ha <- paste(target_self, param_labs["ha"], "*", p2_reports) %>% stringr::str_flatten(collapse = "\n")
-        da <- paste(target_self, param_labs["da"], "*", p1_reports) %>% stringr::str_flatten(collapse = "\n")
+        hc <- paste(p1_reports, "~~", param_labs["hc"], "*", p2_reports) %>% stringr::str_flatten(collapse = "\n")
+        ha <- paste(target_self,"~~",  param_labs["ha"], "*", p2_reports) %>% stringr::str_flatten(collapse = "\n")
+        da <- paste(target_self,"~~",  param_labs["da"], "*", p1_reports) %>% stringr::str_flatten(collapse = "\n")
 
         int_p1 <- paste(p1_reports, "~", param_labs["int_p1"], "*1") %>% stringr::str_flatten(collapse = "\n")
         int_p2 <- paste(p2_reports, "~", param_labs["int_p2"], "*1") %>% stringr::str_flatten(collapse = "\n")
@@ -523,6 +544,113 @@ rep_con_acc_group_mod_builder <- function(p1_reports, p2_reports, target_self,
     if(n_p2s_per_p1s > 1){print("I'm sorry, this function can only handle designs with 1 P1 per P2; check back for changes")}
   }
 }
+
+#'  Reputation Consensus & Accuracy with Group Moderator Model
+#'
+#' This fits a model estimating the possible hearsay reputation parameters
+#' as a multi-group path model given vectors of P1-, P2-, and target self-reports
+#' (vectors of quoted variable names) and a group-level categorical variable.
+#' The baseline model estimates each parameter seperately,
+#' labelling parameters based on the labels of moderator variable.
+#' Those parameters are:
+#' \describe{
+#' \item{hc}{hearsay consensus; the correlation between P1(T) & P2(T)}
+#' \item{ha}{hearsay accuracy; the correation between P2(T) & T(T)}
+#' \item{da}{direct accuracy; the correlation between P1(T) & T(T)}
+#' \item{int_p1}{Intercept for P1(T)}
+#' \item{int_p2}{Intercept for P2(T)}
+#' \item{int_self}{Intercept for T(T)}
+#' \item{v_p1}{variance for P1(T)}
+#' \item{v_p2}{variance for P2(T)}
+#' \item{v_self}{variance for T(T)}
+#' \item{p1_p2_rel_el}{P1-P2 Relative Elevation (i.e., Mean P1(T) - Mean P2(T))}
+#' \item{self_p2_rel_el}{Self-P2 Relative Elevation (i.e., Mean T(T) - Mean P2(T))}
+#' \item{self_p1_rel_el}{Self-P1 Relative Elevation (i.e., Mean T(T) - Mean P1(T))}
+#' }
+#' \emph{If n exchangeable triads > 1:}
+#' \describe{
+#' \item{rec}{direct reciprocity; the correlation between opposit P1(T)s (e.g., A(C) <-> C(A))}
+#' \item{h}{hearsay reciprocity; the correlation between exchangeable P2(T)s (e.g., B(C) <-> D(A))}
+#' \item{m}{unnamed parameter; The correlation between P2(T) and the opposite P1(T) in a group. (e.g., B(C) <-> C(A))}
+#' \item{tru_sim}{True Similarity; the correlation between targets' self-reports. (e.g., A(A) <-> C(C))}
+#' \item{as_sim_3p}{Third-person assumed similarity; correlation between P2(T) and P1's self-report (e.g., B(C) <- A(A))}
+#' \item{as_sim_1p}{First-person assumed similarity (i.e., interpersonal assumed similarity); correlation betweenP1(T) and P1's self-report (e.g., A(C) <-> A(A))}
+#' }
+#' The function can handle up to n exchangeable triads.
+#' @param data The dataframe.
+#' Data should be wide, with a row for every group of participants.
+#' At a minimum, it must contain three columns: one for P1-reports, one for P2-reports, and
+#' one for targets' self-ratings.
+#' @param model Optional. A model from the corresponding ReputationAnalyses model builder function. If this
+#' is supplied, no additional arguments need to be specified.
+#' @param  p1_reports Quoted column names that contain P1 reports,
+#' or ratings made by the person that knows the target directly.
+#' If more than one is supplied, the target-wise order must match the other
+#' rating types.
+#' @param p2_reports Quoted column names that contain P2 reports,
+#' or ratings made by the person that knows the target indirectly through the corresponding P1.
+#' If more than one is supplied, the target-wise order must match the other
+#' rating types.
+#' @param target_self Quoted column names that contain target self-reports.
+#' If more than one is supplied, the target-wise order must match the other
+#' rating types.
+#' @param group_mod Quoted column names that contains group moderator variable.
+#' @param use_labs Logical indicating whether or not to use the group labels to create the parameter labels.
+#' If FALSE, generic labels (grp1 to grpk, where k is the number of groups) are used.
+#' @param n_triads The number of exchangeable triads in each group. By default, this is determined by
+#' counting the number of P1 reports. It is rare that this parameter would need to be changed.
+#' @param n_p1s_per_p2s The number of P1s for every P2. This defaults to 1.
+#' Currently, only values of 1 are supported.
+#' @param n_p2s_per_p1s The number of P2s for every P1;. This defaults to 1.
+#' Currently, only values of 1 are supported.
+#' @param n_p1s_per_ts The number of P1s for every target;. This defaults to 1.
+#' Currently, only values of 1 are supported.
+#' @param n_p2s_per_ts The number of P2s for every target;. This defaults to 1.
+#' Currently, only values of 1 are supported.
+#' @param n_ts_per_p1s The number of targets for every P1;. This defaults to 1.
+#' Currently, only values of 1 are supported.
+#' @param n_ts_per_p2s The number of targets for every P2;. This defaults to 1.
+#' Currently, only values of 1 are supported.
+#'
+#' @import lavaan
+#' @export
+#' @examples
+#' @examples data("rep_sim_data")
+#'           agree_con_acc_grpmod <- rep_con_acc_group_mod(data = rep_sim_data,
+#'                                            p1_reports = c("A_C_agreeableness", "C_A_agreeableness"),
+#'                                            p2_reports = c("B_C_agreeableness", "D_A_agreeableness"),
+#'                                            target_self = c("C_C_agreeableness", "A_A_agreeableness"),
+#'                                            group = "study")
+#'          # alternatively
+#'          # build the model
+#'          agree_con_acc_grpmod_model <- rep_con_acc_group_mod_builder(p1_reports = c("A_C_agreeableness", "C_A_agreeableness"),
+#'                                                              p2_reports = c("B_C_agreeableness", "D_A_agreeableness"),
+#'                                                              group = levels(rep_sim_data$study))
+#'          # then fit it
+#'          agree_con_acc_grpmod <- rep_con_acc_group_mod(data = rep_sim_data,
+#'                                                            model = agree_consensus_model)
+#'
+#'
+#' @return The function returns an object of class \code{\link[lavaan:lavaan-class]{lavaan}}.
+
+rep_con_acc_group_mod <- function(data, model = NULL, p1_reports, p2_reports, target_self,
+                                  group_mod = NULL, use_labs = FALSE, n_triads = length(p1_reports),
+                                    n_p1s_per_p2s = 1, n_p2s_per_p1s = 1){
+  if(is.null(group_mod)){warning("You need to supply a group variable to run a group-moderator Reputation Model.
+                                 If you don't have a group moderator, try rep_consensus if you have no moderator or
+                                 rep_id_mods_consensus if you have an individual difference moderator.")
+  }
+  else{
+    groups <- levels(as.factor(data[,group_mod]))
+    if(is.null(model)){
+      rep_con_acc_group_model <- rep_con_acc_group_mod_builder(p1_reports, p2_reports, target_self,
+                                                               groups,  use_labs = use_labs, n_triads = length(p1_reports),
+                                                               n_p1s_per_p2s = 1, n_p2s_per_p1s = 1)}
+
+    else{rep_con_acc_group_model <- model}
+    fitted_model <- lavaan::sem(rep_con_acc_group_model$model, data = data, missing = "FIML", group = group_mod)
+    return(fitted_model)}
+  }
 
 #' Reputation Consensus, Accuracy, and 3rd-Person Meta-Perception Model Builder
 #' This builds a model of lavaan syntax for estimating the possible hearsay reputation parameters
@@ -823,3 +951,4 @@ rep_full_3pmeta_group_mod_builder <- function(p1_reports, p2_reports, target_sel
     if(n_p2s_per_p1s > 1){print("I'm sorry, this function can only handle designs with 1 P1 per P2; check back for changes")}
   }
   }
+
